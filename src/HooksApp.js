c6111d16e1057c65db0e3277d03c57404f36f5c2
Pablo Arce <arcepablo07@gmail.com>
2022-02-09 17:38:22 -0300

    custom hooks #0902

---
+++ src/HooksApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const HooksApp = (props) => {
+  return (
+  <>
+    <h1>Hola Mundo</h1>
+  </>  
+  )
+};
+
+// HooksApp.propTypes = {};
+
+export default HooksApp;
