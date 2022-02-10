efa2df2a01806f39ca7e8cf478ee6dd409a3a36f
Pablo Arce <arcepablo07@gmail.com>
2022-02-10 15:34:53 -0300

    prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { FormWithCustomHook } from './components/02-useEffect.js/FormWithCustomHook';
-// import HooksApp from './HooksApp' 
+import { FormWithCustomHook } from "./components/02-useEffect.js/FormWithCustomHook";
+// import HooksApp from './HooksApp'
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect.js/SimpleForm';
 
-
-
-ReactDOM.render(
-  <FormWithCustomHook />,  
-  document.getElementById('root')
-);
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
