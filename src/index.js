a93fed0d52cc7f5a7e4f3fd5b1d52c67a2f6868d
Pablo Arce <arcepablo07@gmail.com>
2022-02-10 15:31:59 -0300

    form with custom hook #0210

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { FormWithCustomHook } from './components/02-useEffect.js/FormWithCustomHook';
 // import HooksApp from './HooksApp' 
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect.js/SimpleForm';
+// import { SimpleForm } from './components/02-useEffect.js/SimpleForm';
 
 
 
 ReactDOM.render(
-  <SimpleForm />,  
+  <FormWithCustomHook />,  
   document.getElementById('root')
 );
