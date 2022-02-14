919d1d16ea5804d59ee22236e80a9624c4ace93c
Pablo Arce <arcepablo07@gmail.com>
2022-02-14 09:22:46 -0300

    useFetch useCounter #1402

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { FormWithCustomHook } from "./components/02-useEffect.js/FormWithCustomHook";
 // import HooksApp from './HooksApp'
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect.js/SimpleForm';
+// import { FormWithCustomHook } from "./components/02-useEffect.js/FormWithCustomHook";
+import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById("root"));
