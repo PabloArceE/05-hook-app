fd808ae51cc4b0b660446437d191845ff7d2da05
Pablo Arce <arcepablo07@gmail.com>
2022-02-23 08:18:35 -0300

    todo app template #2302

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,42 +1,60 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from "react";
 
-import todoReducer from './todoReducer'
+import todoReducer from "./todoReducer";
 
-import './styles.css'
+import "./styles.css";
 
-const initialState = [{
+const initialState = [
+  {
     id: new Date().getTime(),
-    desc: 'Estado inicial',
+    desc: "Estado inicial",
     done: false,
-}];
+  },
+];
 
 export const TodoApp = () => {
-
-    const [todos] = useReducer(todoReducer, initialState);
+  const [todos] = useReducer(todoReducer, initialState);
 
   return (
     <>
-        <h1>TodoApp ({ todos.length })</h1>
-        <hr/>
+      <h1>TodoApp ({todos.length})</h1>
+      <hr />
 
-        <div className='col-7'>
-          <ul className='list-group list-group-flush'>
-              {
-                todos.map( (todo, i) => (
-                  <li
-                    key= {todo.id}
-                    className= 'list-group-item'
-                  >
-                    <p className='text-center'>{i + 1}. {todo.desc}</p>  
-                  </li>
-                ))
-              }
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {todo.desc}
+                </p>
+                <button className="btn btn-outline-danger">Borrar</button>
+              </li>
+            ))}
           </ul>
         </div>
 
-        <div className='col-5'>
-          Agregar
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form>
+            <input
+              type="text"
+              name="description"
+              placeholder="Aprender..."
+              autoComplete="off"
+              className="form-control"
+            />
+
+            <div class="d-grid gap-2">
+              <button className="btn btn-outline-primary mt-1">
+                Agregar
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
